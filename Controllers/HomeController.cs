0aad75dae9d970a16a0e34a436cb6ba7a96a024a
Sobhie <sobhie.saad1@gmail.com>
2022-01-18 23:12:22 +0300

    Employee management system with chart data analyzing

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChartsInMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public static DataTable GetVehicleSummary()
+        {
+            DataTable dt = new DataTable("VehicleSummary");
+            string query = "Select Vehicletype,str(count(Vehicletype)* 100.0 / (Select Count(*) From VehicleMaster), 5,1) as percentage ";
+            query+="from VehicleMaster group by Vehicletype";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=.;" + "Initial Catalog=Transport;" + "Integrated Security=True;";
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = query;
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        public class Summary
+        {
+            public double Item { get; set; }            public string Value { get; set; }
+        }
+
+        [HttpGet]
+        public JsonResult VehicleSummary()
+        {
+            List<Summary> lstSummary = new List<Summary>();
+            foreach (DataRow dr in GetVehicleSummary().Rows)
+            {
+                Summary summary = new Summary();
+                summary.Value = dr[0].ToString().Trim();
+                summary.Item = Convert.ToDouble(dr[1]);
+                lstSummary.Add(summary);
+            }
+            return Json(lstSummary.ToList(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChartsInMvc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChartsInMvc")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65843b0a-24b5-4dbe-9b77-2006f37142f3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
